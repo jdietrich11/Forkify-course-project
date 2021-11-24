b40594f739162ac8b2be77458af3bb9cc6197671
jdietrich11 <jbdietrich2@gmail.com>
2021-11-24 00:51:50 -0700

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9c90351a-0bad-4765-bd6f-3d65bd006e23';
+export const MODAL_CLOSE_SEC = 2.5;
